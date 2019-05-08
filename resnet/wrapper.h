397b3309df29ef5fadda67f03ce33c1631415678
fluorinedog <fluorinedog@gmail.com>
2019-05-08 15:01:45 +0800

    tmp

--- resnet/wrapper.h
+++ resnet/wrapper.h
@@ -2,10 +2,21 @@
 class Dims : public std::vector<int> {
   public:
     using std::vector<int>::vector;
-    operator int*(){
+    operator int*() {
         return this->data();
     }
-    operator int*() const{
+    operator int*() const {
         return *this;
     }
 };
+
+class DeviceVector : public thrust::device_vector<T> {
+  public:
+    using thrust::device_vector<T>::device_vector;
+    operator T*() {
+        return this->data().get();
+    }
+    operator T*() const {
+        return *this;
+    }
+}
