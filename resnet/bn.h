20a6467746c0a70ad06a7164fe25f127383a60c1
fluorinedog <fluorinedog@gmail.com>
2019-05-11 15:46:50 +0800

    mv

---
+++ resnet/bn.h
@@ -0,0 +1,52 @@
+#include "functor.h"
+class BatchNorm {
+  public:
+    static constexpr auto kMode = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
+    BatchNorm(dim_t dims) : dsc_io(dims), fwd_counter(0) {
+        cudnnDeriveBNTensorDescriptor(dsc_bn, dsc_io, kMode);
+        this->bn_size = get_volume(dsc_bn.dims);
+        extra.resize(bn_size * 4);
+        thrust::fill_n(extra.begin(), extra.size(), 0);
+    }
+    void forward(void* out, const void* in, const void* weight) {
+        float alpha = 1, beta = 0;
+        auto bnScale = (T*)weight;
+        auto bnBias = 1 * bn_size + (T*)weight;
+        auto bnRunningMean = (T*)extra;
+        auto bnRunningVar = 1 * bn_size + (T*)extra;
+        auto bnSavedMean = 2 * bn_size + (T*)extra;
+        auto bnSavedVar = 3 * bn_size + (T*)extra;
+        ++fwd_counter;
+        cudnnBatchNormalizationForwardTraining(
+            global.get_handle(), kMode, &alpha, &beta, dsc_io, in, dsc_io, out, dsc_bn,
+            bnScale, bnBias, 1.0 / fwd_counter, bnRunningMean, bnRunningVar,
+            CUDNN_BN_MIN_EPSILON, bnSavedMean, bnSavedVar);
+    }
+    size_t weight_size() {
+        return 2 * bn_size;
+    }
+    void backward(void* in_grad, void* weight_grad, const void* in, const void* out_grad,
+                  const void* weight) {
+        float alpha = 1, beta = 0;
+        auto bnScale = (T*)weight;
+        auto bnBias = 1 * bn_size + (T*)weight;
+        auto bnScale_grad = (T*)weight_grad;
+        auto bnBias_grad = 1 * bn_size + (T*)weight_grad;
+        // auto bnRunningMean = (T*)extra;
+        // auto bnRunningVar = 1 * bn_size + (T*)extra;
+        auto bnSavedMean = 2 * bn_size + (T*)extra;
+        auto bnSavedVar = 3 * bn_size + (T*)extra;
+        cudnnBatchNormalizationBackward(
+            global.get_handle(), kMode, &alpha, &beta, &alpha, &beta, dsc_io, in, dsc_io,
+            out_grad, dsc_io, in_grad, dsc_bn, bnScale, bnScale_grad, bnBias_grad,
+            CUDNN_BN_MIN_EPSILON, bnSavedMean, bnSavedVar);
+    }
+    
+  private:
+    TensorDescriptor dsc_io;
+    TensorDescriptor dsc_bn;
+    DeviceVector<T> extra;
+    size_t fwd_counter;
+    size_t bn_size;
+};
+
