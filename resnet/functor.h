927dd7fbc462192b3411ee785fa72e2344a2ff89
fluorinedog <fluorinedog@gmail.com>
2019-05-11 15:26:08 +0800

    brain stop

--- resnet/functor.h
+++ resnet/functor.h
@@ -24,21 +24,35 @@ class FunctorBase {
 
 class BatchNorm : public FunctorBase {
   public:
-    BatchNorm(dim_t dims) : io_dsc(dims), bn_dsc({1, dims[1], 1, 1}) {}
+    static constexpr auto kMode = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
+    BatchNorm(dim_t dims) : io_dsc(dims), fwd_counter(0) {
+        cudnnDeriveBNTensorDescriptor(bn_dsc, io_dsc, kMode);
+        this->bn_size = get_volume(bn_dsc.dims);
+        extra.resize(bn_size * 4);
+        thrust::fill_n(extra.begin(), extra.size(), 0);
+    }
     void forward(void* out, const void* in, const void* weight) override {
-        auto kMode = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
         float alpha = 1, beta = 0;
         auto bnScale = (T*)weight;
-        auto bnBias = bn_dsc.dims[1] + (T*)weight;
-        cudnnBatchNormalizationForwardTraining(global.get_handle(), kMode, &alpha, &beta,
-                                               io_dsc, in, io_dsc, out, bn_dsc, bnScale, bnBias, );
+        auto bnBias = 1 * bn_size + (T*)weight;
+        auto bnRunningMean = (T*)extra;
+        auto bnRunningVar = 1 * bn_size + (T*)extra;
+        auto bnSavedMean = 2 * bn_size + (T*)extra;
+        auto bnSavedVar = 3 * bn_size + (T*)extra;
+        ++fwd_counter;
+        cudnnBatchNormalizationForwardTraining(
+            global.get_handle(), kMode, &alpha, &beta, io_dsc, in, io_dsc, out, bn_dsc,
+            bnScale, bnBias, 1.0 / fwd_counter, bnRunningMean, bnRunningVar,
+            CUDNN_BN_MIN_EPSILON, bnSavedMean, bnSavedVar);
     }
     size_t weight_size() override {
-        dim_t dims = dsc.dims();
-        return 2 * dims[1] * sizeof(T);
+        return 2 * bn_size;
     }
-    void backwardData(void* in_grad, const void* out_grad, const void* weight) = 0;
-    void backwardFilter(void* weight_grad, const void* out_grad, const void* in) = 0;
+    void backwardData(void* in_grad, const void* out_grad, const void* weight){
+        float alpha = 1, beta = 0;
+         
+    }
+    void backwardFilter(void* weight_grad, const void* out_grad, const void* in){};
     size_t workspace_fwd() {
         return 0;
     }
@@ -52,4 +66,7 @@ class BatchNorm : public FunctorBase {
   private:
     TensorDescriptor io_dsc;
     TensorDescriptor bn_dsc;
+    DeviceVector<T> extra;
+    size_t fwd_counter;
+    size_t bn_size;
 };
