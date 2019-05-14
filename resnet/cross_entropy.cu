cbf1d4b3aaccd144f502128fcc6dac155b31d55a
fluorinedog <fluorinedog@gmail.com>
2019-05-14 18:52:31 +0800

    after cmp

--- resnet/cross_entropy.cu
+++ resnet/cross_entropy.cu
@@ -1,20 +1,19 @@
 #include "cross_entropy.h"
 #define PAR(total, threads) <<<((total) + threads - 1) / threads, threads>>>
 
-
-__global__
-void nll_loss(float *loss, const float *logits_grad, const int *labels, int C, int N) {
+__global__ void nll_loss(float *loss, const float *logits_grad, const int *labels, int C,
+                         int N) {
     int index = threadIdx.x + blockIdx.x * blockDim.x;
-    if (index < N) {
+    if(index < N) {
         int class_id = labels[index];
         atomicAdd(loss, logits_grad[index * C + class_id]);
     }
 }
 
-__global__
-void nll_loss_backward(float* logits_grad, float final, const int* labels, int C, int N){
+__global__ void nll_loss_backward(float *logits_grad, float final, const int *labels,
+                                  int C, int N) {
     int index = threadIdx.x + blockIdx.x * blockDim.x;
-    if (index < N * C) {
+    if(index < N * C) {
         logits_grad[index] = 0.0;
     }
     if(index < N) {
@@ -23,13 +22,13 @@ void nll_loss_backward(float* logits_grad, float final, const int* labels, int C
     }
 }
 
-
 void CrossEntropy::forward(float *loss, const float *act, const int *labels) {
     auto kAlgo = CUDNN_SOFTMAX_LOG;
     auto kMode = CUDNN_SOFTMAX_MODE_INSTANCE;
     float one = 1.0, zero = 0.0;
-    auto logits = (float *) global.get_workspace();
-    cudnnSoftmaxForward(global.cudnn_handle(), kAlgo, kMode, &one, dsc_io, act, &zero, dsc_io, logits);
+    auto logits = (float *)global.get_workspace();
+    cudnnSoftmaxForward(global.cudnn_handle(), kAlgo, kMode, &one, dsc_io, act, &zero,
+                        dsc_io, logits);
     nll_loss PAR(batch_size, 128)(loss, logits, labels, class_size, batch_size);
     // nll_loss
 }
@@ -38,13 +37,10 @@ void CrossEntropy::backward(float *act_grad, float loss_grad, const int *labels)
     auto kAlgo = CUDNN_SOFTMAX_LOG;
     auto kMode = CUDNN_SOFTMAX_MODE_INSTANCE;
     float one = 1.0, zero = 0.0;
-    auto logits = (float *) global.get_workspace();
+    auto logits = (float *)global.get_workspace();
     // nll_loss
-    nll_loss_backward PAR(class_size*batch_size, 128)(logits, loss_grad, labels, class_size, batch_size);
-    cudnnSoftmaxBackward(global.cudnn_handle(), kAlgo, kMode, &one, dsc_io, global.get_workspace(), dsc_io,
-                         logits, &zero, dsc_io, act_grad);
-
+    nll_loss_backward PAR(class_size * batch_size, 128)(logits, loss_grad, labels,
+                                                        class_size, batch_size);
+    cudnnSoftmaxBackward(global.cudnn_handle(), kAlgo, kMode, &one, dsc_io,
+                         global.get_workspace(), dsc_io, logits, &zero, dsc_io, act_grad);
 }
-
-
-
--- resnet/cross_entropy.h
+++ resnet/cross_entropy.h
@@ -3,14 +3,12 @@
 #include "global.h"
 #include "descriptor.h"
 
-
-
-
 class CrossEntropy {
-public:
-    CrossEntropy(int class_size, int batch_size) : class_size(class_size), batch_size(batch_size),
-                                                          dsc_io({batch_size, class_size, 1, 1}) {
-    }
+  public:
+    CrossEntropy(int class_size, int batch_size)
+        : class_size(class_size),
+          batch_size(batch_size),
+          dsc_io({batch_size, class_size, 1, 1}) {}
 
     void forward(float *loss, const float *act, const int *labels);
 
@@ -20,9 +18,8 @@ public:
         return class_size * batch_size * sizeof(float);
     }
 
-private:
+  private:
     int class_size;
     int batch_size;
     TensorDescriptor dsc_io;
 };
-
