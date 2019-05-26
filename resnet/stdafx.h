5c947769ae8c909b315d5a5b3f7ad5f4dbb051e9
fluorinedog <fluorinedog@gmail.com>
2019-05-26 14:46:52 +0800

    fucker

--- resnet/stdafx.h
+++ resnet/stdafx.h
@@ -12,29 +12,6 @@ struct functor {
     }
 };
 
-// void dog_print(std::string name, const float* ptr, const dim_t& dim) {
-//     cout << name << endl;
-//     auto sz = get_volume(dim);
-
-//     cudaDeviceSynchronize();
-//     host_vector<T> vec(sz);
-//     cudaMemcpy(vec.data(), ptr, sz * sizeof(float), cudaMemcpyDefault);
-//     auto tmp = dim;
-//     std::reverse(tmp.begin(), tmp.end());
-//     for(auto index : Range(sz)) {
-//         int index_cpy = index;
-//         for(auto x : tmp) {
-//             if(index_cpy % x != 0) break;
-//             index_cpy /= x;
-//             cout << "--------" << endl;
-//         }
-//         cout << vec[index] << " ";
-//     }
-//     cout << endl << "##########" << endl;
-// }
-
-
-
 float get_acc(float* dev_logits, int* labels, int N, int feature);
 host_vector<int> get_labels();
 host_vector<float> get_data();
