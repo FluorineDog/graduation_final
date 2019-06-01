c8a0cd180f17e165659899bf90967a9913680fd4
fluorinedog <fluorinedog@gmail.com>
2019-06-01 12:46:00 +0800

    update

--- resnet/resnet.h
+++ resnet/resnet.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "stdafx.h"
-auto ref(Engine& eng, int x, int classes) {
+int naive_net(Engine& eng, int x_, int classes) {
+    auto x = x_;
     x = eng.insert_node<ConvolutionNode>(
         x, /*C_out*/ 32,
         /*kernel*/ 3, /*group*/ 1, /*padding*/ 1,
@@ -27,64 +28,72 @@ auto ref(Engine& eng, int x, int classes) {
     // x = eng.insert_node<ActivationNode>(x);
 
     x = eng.insert_node<FCNode>(x, classes);
+    return x;
 }
 
-// int create_bottleneck(
-//     Engine& eng, int x_, int B, int hw_, int in_planes, int planes, int stride) {
-//     int c_in, hw, c_out;
+int create_bottleneck(Engine& eng, int x_, int in_planes, int planes, int stride) {
+    auto x = x_;
+    x = eng.insert_node<ConvolutionNode>(
+        x, /*C_out*/ planes,
+        /*kernel*/ 1, /*group*/ 1, /*padding*/ 0,
+        /*stride*/ 1, /*dilation*/ 1);
+    x = eng.insert_node<BatchNormNode>(x);
+    x = eng.insert_node<ActivationNode>(x);
 
-//     c_in = in_planes;
-//     c_out = planes;
-//     hw = hw_;
-//     auto x = x_;
-//     x = eng.insert_node<ConvolutionNode>(
-//         x, dim_t{B, c_in, hw, hw}, /*C_out*/ c_out,
-//         /*kernel*/ 1, /*group*/ 1, /*padding*/ 0,
-//         /*stride*/ 1, /*dilation*/ 1);
-//     c_in = c_out;
-//     x = eng.insert_node<BatchNormNode>(x, dim_t{B, c_out, hw, hw});
-//     x = eng.insert_node<ActivationNode>(x, dim_t{B, c_out, hw, hw});
+    x = eng.insert_node<ConvolutionNode>(
+        x, /*C_out*/ planes,
+        /*kernel*/ 3, /*group*/ 1, /*padding*/ 1,
+        /*stride*/ stride, /*dilation*/ 1);
+    x = eng.insert_node<BatchNormNode>(x);
+    x = eng.insert_node<ActivationNode>(x);
+    x = eng.insert_node<ConvolutionNode>(
+        x, /*C_out*/ 4 * planes,
+        /*kernel*/ 1, /*group*/ 1, /*padding*/ 0,
+        /*stride*/ 1, /*dilation*/ 1);
+    auto shortcut = x_;
 
-//     x = eng.insert_node<ConvolutionNode>(
-//         x, dim_t{B, c_in, hw, hw}, /*C_out*/ c_out,
-//         /*kernel*/ 3, /*group*/ 1, /*padding*/ 1,
-//         /*stride*/ stride, /*dilation*/ 1);
-//     hw = (hw + 2 * 1 - 3) / stride + 1;
-//     x = eng.insert_node<BatchNormNode>(x, dim_t{B, c_out, hw, hw});
-//     x = eng.insert_node<ActivationNode>(x, dim_t{B, c_out, hw, hw});
-//     c_out = 4 * c_in;
-//     x = eng.insert_node<ConvolutionNode>(
-//         x, dim_t{B, c_in, hw, hw}, /*C_out*/ c_out,
-//         /*kernel*/ 1, /*group*/ 1, /*padding*/ 0,
-//         /*stride*/ 1, /*dilation*/ 1);
-//     c_in = c_out;
-//     auto shortcut = x_;
-//     if(stride != 1 || in_planes != 4 * planes) {
-//         auto c_in = in_planes;
-//         auto c_out = 4 * planes;
-//         auto hw = hw_;
-//         shortcut = eng.insert_node<ConvolutionNode>(
-//             shortcut, dim_t{B, c_in, hw, hw}, /*C_out*/ c_out,
-//             /*kernel*/ 1, /*group*/ 1, /*padding*/ 0,
-//             /*stride*/ stride, /*dilation*/ 1);
-//         c_in = c_out;
-//         hw = (hw - 1) / stride + 1;
-//         shortcut = eng.insert_node<BatchNormNode>(shortcut, dim_t{B, c_out, hw, hw});
-//     }
-//     x = eng.insert_blend<AddNode>(x, shortcut, dim_t{B, c_in, hw, hw});
-//     x = eng.insert_node<ActivationNode>(x, dim_t{B, c_in, hw, hw});
-//     return x;
-// }
+    if(stride != 1 || in_planes != 4 * planes) {
+        shortcut = eng.insert_node<ConvolutionNode>(
+            shortcut, /*C_out*/ 4 * planes,
+            /*kernel*/ 1, /*group*/ 1, /*padding*/ 0,
+            /*stride*/ stride, /*dilation*/ 1);
+        shortcut = eng.insert_node<BatchNormNode>(shortcut);
+    }
+    x = eng.insert_blend<AddNode>(x, shortcut);
+    x = eng.insert_node<ActivationNode>(x);
+    return x;
+}
 
-// void make_layer(Engine& eng, int x, int in_planes, int planes, int n_blocks, int stride_){
-//     std::vector<int> strides(n_blocks, 1);
-//     strides[0] = stride_;
-//     for(auto stride: strides){
+std::pair<int, int> make_layer(
+    Engine& eng, int x_, int in_planes_,    //
+    int planes, int n_blocks, int stride_) {
+    auto x = x_;
+    auto in_planes = in_planes_;
+    auto stride = stride_;
+    for(auto i : Range(n_blocks)) {
+        x = create_bottleneck(eng, x, in_planes, planes, stride);
+        in_planes = 4 * planes;
+        stride = 1;
+    }
+    return std::make_pair(x, in_planes);
+}
 
-//     }
-
-// }
-
-// void construct_resnet(Engine& eng) {
-
-// }
+int construct_resnet(Engine& eng, int x_, std::vector<int> blocks, int classes) {
+    assert(blocks.size() == 4);
+    auto x = x_;
+    auto init_planes = 64;
+    x = eng.insert_node<ConvolutionNode>(
+        x, /*C_out*/ init_planes,
+        /*kernel*/ 3, /*group*/ 1, /*padding*/ 1,
+        /*stride*/ 1, /*dilation*/ 1);
+    x = eng.insert_node<BatchNorm>(x);
+    x = eng.insert_node<ActivationNode>(x);
+    int in_planes = 64;
+    std::tie(x, in_planes) = make_layer(eng, x, in_planes, 64, blocks[0], 1);
+    std::tie(x, in_planes) = make_layer(eng, x, in_planes, 128, blocks[0], 2);
+    std::tie(x, in_planes) = make_layer(eng, x, in_planes, 256, blocks[0], 2);
+    std::tie(x, in_planes) = make_layer(eng, x, in_planes, 512, blocks[0], 2);
+    x = eng.insert_node<PoolingNode>(x, /*kernel*/ 3, /*padding*/ 1, /*stride*/ 1);
+    x = eng.insert_node<FCNode>(x, classes);
+    return x;
+}
