6412225b16c58666670a4f17e3bb38f51845075c
fluorinedog <fluorinedog@gmail.com>
2019-06-02 15:19:52 +0800

    split GradientManager

--- resnet/memory_manager.h
+++ resnet/memory_manager.h
@@ -1,36 +1,24 @@
 #pragma once
 #include "helper/common.h"
 #include "helper/defs.h"
-class MemoryManager {
-  public:
-    void init() {
-        //
-    }
-    void register_feature_map(int id, size_t size) {
-        assert(!feature_mapping.count(id));
-        feature_mapping[id].resize(size);
-    }
-    void register_gradient_map(int id, size_t size) {
-        assert(!gradient_mapping.count(id));
-        gradient_mapping[id].resize(size);
-    }
-    float* get_feature(int id) {
-        assert(feature_mapping.count(id));
-        return feature_mapping[id];
-    }
 
+class GradientManager {
+  public:
     float* get_gradient(int id) {
         assert(gradient_mapping.count(id));
         return gradient_mapping[id];
     }
 
+    void register_gradient_map(int id, size_t size) {
+        assert(!gradient_mapping.count(id));
+        gradient_mapping[id].resize(size);
+    }
     const float* get_gradient_final(int id) {
         assert(gradient_mapping.count(id));
         return gradient_mapping[id];
     }
-
     void zero_grad() {
-        #pragma omp parallel for
+#pragma omp parallel for
         for(auto& pr : gradient_mapping) {
             // if(pr.first > 0){
             //     break;
@@ -42,14 +30,33 @@ class MemoryManager {
             // assert(inspect(ptr) == 0);
         }
     }
+    void terminate(){
+        gradient_mapping.clear();
+    }
+  private:
+    std::map<int, DeviceVector<float>> gradient_mapping;
+};
 
-    void free(int id) {}
+class MemoryManager: public GradientManager {
+  public:
+    void init() {
+        //
+    }
+    void register_feature_map(int id, size_t size) {
+        assert(!feature_mapping.count(id));
+        feature_mapping[id].resize(size);
+    }
+
+    float* get_feature(int id) {
+        assert(feature_mapping.count(id));
+        return feature_mapping[id];
+    }
+
     void terminate() {
         feature_mapping.clear();
-        gradient_mapping.clear();
+        GradientManager::terminate();
     }
 
   private:
     std::map<int, DeviceVector<float>> feature_mapping;
-    std::map<int, DeviceVector<float>> gradient_mapping;
 };
