570cb834d1cb4e43dc4c4788a42e389f625569ae
fluorinedog <fluorinedog@gmail.com>
2019-06-02 15:05:20 +0800

    isolate memory_manager

--- resnet/computational_graph.h
+++ resnet/computational_graph.h
@@ -1,66 +1,12 @@
 #pragma once
-#include "helper/defs.h"
-#include "helper/common.h"
+#include "memory_manager.h"
 #include "../doglib/graph/graph.h"
 #include "../doglib/graph/procedure.h"
 #include <random>
 #include "optimizer.h"
 #include "visitors.h"
-//  stupid version
-//  definition:
-//    accept correct
-class MemoryManager {
-  public:
-    void init() {
-        //
-    }
-    void register_feature_map(int id, size_t size) {
-        assert(!feature_mapping.count(id));
-        feature_mapping[id].resize(size);
-    }
-    void register_gradient_map(int id, size_t size) {
-        assert(!gradient_mapping.count(id));
-        gradient_mapping[id].resize(size);
-    }
-    float* get_feature(int id) {
-        assert(feature_mapping.count(id));
-        return feature_mapping[id];
-    }
 
-    float* get_gradient(int id) {
-        assert(gradient_mapping.count(id));
-        return gradient_mapping[id];
-    }
 
-    const float* get_gradient_final(int id) {
-        assert(gradient_mapping.count(id));
-        return gradient_mapping[id];
-    }
-
-    void zero_grad() {
-        #pragma omp parallel for
-        for(auto& pr : gradient_mapping) {
-            // if(pr.first > 0){
-            //     break;
-            // }
-            auto& vec = pr.second;
-            auto ptr = vec.data().get();
-            thrust::fill(thrust::device, vec.begin(), vec.end(), 0);
-            // cudaMemset(vec.data().get(), 0, sizeof(float) * vec.size());
-            // assert(inspect(ptr) == 0);
-        }
-    }
-
-    void free(int id) {}
-    void terminate() {
-        feature_mapping.clear();
-        gradient_mapping.clear();
-    }
-
-  private:
-    std::map<int, DeviceVector<float>> feature_mapping;
-    std::map<int, DeviceVector<float>> gradient_mapping;
-};
 
 // graph executor, in one place
 class Engine {
